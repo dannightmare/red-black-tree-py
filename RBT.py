6b3c8a92d078e7a10c89f54ac8981a73db2a2862
Daniel Kusai <dan.kusai@gmail.com>
2025-03-02 22:30:48 +0200

    added left_rotate and its test

--- RBT.py
+++ RBT.py
@@ -19,12 +19,10 @@ class red_black_tree:
             print("Duplicate val")
         elif val < self.val:
             self.left = self.left.insert(
-                val) if self.left else red_black_tree(val)
-            self.left.parent = self
+                val) if self.left else red_black_tree(val, parent=self)
         else:
             self.right = self.right.insert(
-                val) if self.right else red_black_tree(val)
-            self.right.parent = self
+                val) if self.right else red_black_tree(val, parent=self)
         return self
 
     def preorder(self):
@@ -33,3 +31,35 @@ class red_black_tree:
             self.left.preorder()
         if self.right:
             self.right.preorder()
+
+    def left_rotate(self):
+        x = self.right
+        if x is None:
+            print("Can't rotate left")
+            return self
+
+        x.parent = self.parent
+        self.right = x.left
+        if self.right:
+            self.right.parent = self
+
+        x.left = self
+        self.parent = x
+
+        return x
+
+
+def test_left_rotate_1():
+    t = red_black_tree()
+    t.insert(1)
+    t.insert(0)
+    t.insert(2)
+
+    t = t.left_rotate()
+
+    assert (t.val == 2)
+    assert (t.left.val == 1)
+    assert (t.left.left.val == 0)
+
+    assert (t.left.parent == t)
+    assert (t.left.left.parent == t.left)
